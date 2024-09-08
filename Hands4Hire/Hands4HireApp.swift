ed2c0a5ab7911a4981c549fd715dec774e3b15c6
Nouman Gul <Noumanguljunejo@gmail.com>
2024-09-08 11:16:38 +0400

    Initial Commit

---
+++ Hands4Hire/Hands4HireApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hands4HireApp.swift
+//  Hands4Hire
+//
+//  Created by Nouman Gul Junejo on 08/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Hands4HireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
